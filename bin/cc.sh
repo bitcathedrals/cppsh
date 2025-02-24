7fd9b8fa84b24f996839280fd7e4c095bcddf947
Michael Mattie <codermattie@runbox.com>
2025-02-24 11:28:40 -0700

    (feat): seperate install of tools into macos, ubuntu, and kali. Add tools

--- bin/cc.sh
+++ bin/cc.sh
@@ -1,19 +1,24 @@
 #! /usr/bin/env bash
 
+debian_based="clang lldb gdb binutils autoconf build2"
 
 case $1 in
   "macos-tools")
-    brew install bear build2
+    brew install bear build2 binutils
     ;;
-  "macos-reverse")
-    brew install binutils
+  "ubuntu")
+      eval "doas apt install $debian_based"
+    ;;
+  "kali")
+      eval "doas apt insatll $debian_based imhex"
     ;;
   *)
 cat <<HELP
 cc.sh
 
-macos-tools   = install macos cpp tools using brew
-macos-reverse = install reverse engineering tools
+macos-tools   = macos tools install
+ubuntu        = ubuntu tools install
+kali          = kali tools install
 HELP
     ;;
 esac
